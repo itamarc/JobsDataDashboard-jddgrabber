28145e6790b6f58d4c96656d103a361a9a0dc6fd
Itamar Carvalho <itamarc@gmail.com>
2021-05-25 20:47:10 -0300

    Working on DataStore and unittest

--- test/test_JDDConfig.py
+++ test/test_JDDConfig.py
@@ -1,16 +1,18 @@
+import unittest
 from recursive_diff import recursive_eq
 import jddgrabber.JDDConfigFileCreator as fc
 import jddgrabber.JDDConfig as conf
 
 
-def test_create_load_config():
-    config_file = r'config-test.yaml'
-    default_config = fc.get_default_config()
-    fc.create_config_file(config_file)
-    config = conf.load_config(config_file)
-    recursive_eq(default_config, config)
-    fc.remove_config_file(config_file)
+class test_JDDConfig(unittest.TestCase):
+    def test_create_load_config(self):
+        config_file = r'config-test.yaml'
+        default_config = fc.get_default_config()
+        fc.create_config_file(config_file)
+        config = conf.load_config(config_file)
+        recursive_eq(default_config, config)
+        fc.remove_config_file(config_file)
 
 
-if __name__ == '__main__':
-    test_create_load_config()
+# if __name__ == '__main__':
+#     test_create_load_config()
